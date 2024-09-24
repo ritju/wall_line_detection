760a6b11dfd2382a0dea50600390f8619efe1f74
zxx-0717 <1024988917@qq.com>
2024-09-24 19:50:31 +0800

    墙线检测初步筛选合并

--- wall_line_detection/include/wall_line_detection/wall_line_detection.hpp
+++ wall_line_detection/include/wall_line_detection/wall_line_detection.hpp
@@ -23,10 +23,19 @@ namespace wall_line_detection_pkg
 using LaserScanMsg = sensor_msgs::msg::LaserScan;
 using OccupancyGridMsg = nav_msgs::msg::OccupancyGrid;
 
+
+
 struct MapPose
 {
         int x;
         int y;
+};
+
+struct LineInfo
+{
+        cv::Vec4i line;
+        double rho;
+        double theta;
 };
 
 /**
@@ -67,6 +76,17 @@ int laserscan_queue_size_;
 std::string map_frame;
 std::string base_link_frame;
 std::string laser_link_frame;
+double canny_thr1;
+double canny_thr2;
+double hough_rho;
+double hough_theta;
+int hough_thr;
+double hough_min_line_length;
+double hough_max_line_gap;
+int imshow_wait;
+double vertical_thr;
+int horizontal_thr;
+double theta_thr;
 
 // member functions
 typedef std::recursive_mutex mutex_t;
@@ -110,6 +130,30 @@ void get_map_robot_tf();
 */
 MapPose word_to_picture_bounded(double w_x, double w_y);
 
+double area(int x1, int y1, int x2, int y2, int x3, int y3)
+{
+        return fabs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+}
+
+double distance(int x1, int y1, int x2, int y2)
+{     
+        return std::sqrt(std::pow(x1-x2, 2) + std::pow(y1-y2, 2));
+}
+
+double calculate_vertical_distance_dummy(cv::Vec4i line1, cv::Vec4i line2)
+{
+        int x1, y1, x2, y2;
+        x1 = (line1[0] + line1[2]) / 2;
+        y1 = (line1[1] + line1[3]) / 2;
+        x2 = (line2[0] + line2[2]) / 2;
+        y2 = (line2[1] + line2[3]) / 2;
+        return distance(x1, y1, x2, y2);
+}
+
+void lines_filter(std::vector<LineInfo> &line_infos);
+
+bool merge_lines(cv::Point2i& pt1, cv::Point2i& pt2, cv::Point2i pt3, cv::Point2i pt4, double theta);
+
 // member variables
 OccupancyGridMsg map_grid_;
 LaserScanMsg laserscan_input_current_;
@@ -117,9 +161,10 @@ LaserScanMsg laserscan_output_;
 std::queue<LaserScanMsg> laserscan_queue_;
 mutex_t * laserscan_queue_access_;
 cv::Mat img_map_;
+cv::Mat img_map_empty_;
 int map_width_;
 int map_height_;
-int map_resolution_;
+double map_resolution_;
 geometry_msgs::msg::Pose map_origin_;
 
 // tf2
@@ -129,6 +174,17 @@ tf2::Transform map_robot_tf;
 tf2::Transform map_laser_link_tf;
 
 std::vector<MapPose> laserscan_points_vector;
+
+std::vector<cv::Scalar> colors = 
+{
+        cv::Scalar(0, 255, 0),
+        cv::Scalar(255, 0, 0),
+        cv::Scalar(255, 255, 0),
+        cv::Scalar(255, 0, 255),
+        cv::Scalar(0, 255, 255)
+};
+
+std::vector<LineInfo> lines_;
 
 }; // end of class
 
--- wall_line_detection/src/wall_line_detection.cpp
+++ wall_line_detection/src/wall_line_detection.cpp
@@ -54,6 +54,17 @@ void WallLineDetection::init_params()
         this->declare_parameter<std::string>("base_link_frame", "base_link");
         this->declare_parameter<std::string>("laser_link_frame", "laser_link");
         this->declare_parameter<std::string>("map_frame", "map");
+        this->declare_parameter<double>("canny_thr1", 100.0);
+        this->declare_parameter<double>("canny_thr2", 200.0);
+        this->declare_parameter<double>("hough_rho", 1.0);
+        this->declare_parameter<double>("hough_theta", M_PI / 180.0);
+        this->declare_parameter<int>("hough_thr", 80);
+        this->declare_parameter<double>("hough_min_line_length", 30.0);
+        this->declare_parameter<double>("hough_max_line_gap", 10.0);
+        this->declare_parameter<int>("imshow_wait", 10);
+        this->declare_parameter<double>("vertical_thr", 5.0);
+        this->declare_parameter<int>("horizontal_thr", 5);
+        this->declare_parameter<double>("theta_thr", 0.087266);
 
         this->laserscan_topic_sub_name = this->get_parameter_or<std::string>("laserscan_topic_sub_name", std::string("/scan"));
         this->laserscan_topic_pub_name = this->get_parameter_or<std::string>("laserscan_topic_pub_name", std::string("/scan_converged"));
@@ -62,6 +73,17 @@ void WallLineDetection::init_params()
         this->base_link_frame = this->get_parameter_or<std::string>("base_link_frame", "base_link");
         this->laser_link_frame = this->get_parameter_or<std::string>("laser_link_frame", "laser_link");
         this->map_frame = this->get_parameter_or<std::string>("map_frame", "map");
+        this->canny_thr1 = this->get_parameter_or<double>("canny_trh1", 100.0);
+        this->canny_thr2 = this->get_parameter_or<double>("canny_trh2", 200.0);
+        this->hough_rho = this->get_parameter_or<double>("hough_rho", 1.0);
+        this->hough_theta = this->get_parameter_or<double>("hough_theta", M_PI / 180.0);
+        this->hough_thr = this->get_parameter_or<int>("hough_thr", 80);
+        this->hough_min_line_length = this->get_parameter_or<double>("hough_min_line_length", 30.0);
+        this->hough_max_line_gap = this->get_parameter_or<double>("hought_max_line_gap", 10.0);
+        this->imshow_wait = this->get_parameter_or<int>("imshow_wait", 10);
+        this->vertical_thr = this->get_parameter_or<double>("vertical_thr", 5.0);
+        this->horizontal_thr = this->get_parameter_or<int>("horizontal_thr", 5.0);
+        this->theta_thr = this->get_parameter_or<double>("theta_thr", 0.087266);
 }
 
 void WallLineDetection::get_map_laser_link_tf()
@@ -140,10 +162,11 @@ void WallLineDetection::laserscan_sub_callback_(const LaserScanMsg::SharedPtr ms
                 this->laserscan_output_ = this->process_laserscan_queue(this->laserscan_queue_);
                 this->laserscan_pub_->publish(this->laserscan_output_);
                 std::vector<MapPose>().swap(this->laserscan_points_vector);
-                double yaw = this->laserscan_output_.range_min;
+                double yaw = this->laserscan_output_.angle_min;
                 double yaw_delta = this->laserscan_output_.angle_increment;
                 for (int i = 0; i < (int) this->laserscan_output_.ranges.size(); i++, yaw += yaw_delta)
                 {
+                        // RCLCPP_INFO(this->get_logger(), "yaw: %f", yaw);
                         double range = laserscan_output_.ranges[i];
                         if(std::isinf(range) || range < laserscan_output_.range_min || range > laserscan_output_.range_max)
                         {
@@ -162,23 +185,79 @@ void WallLineDetection::laserscan_sub_callback_(const LaserScanMsg::SharedPtr ms
                                 w_y = tf_map_point.getOrigin().getY();
                                 MapPose map_pose = this->word_to_picture_bounded(w_x, w_y);
                                 this->laserscan_points_vector.push_back(map_pose);
-                                RCLCPP_INFO(this->get_logger(), "x: %d, y: %d", map_pose.x, map_pose.y);
+                                // RCLCPP_INFO(this->get_logger(), "range: %f, yaw: %f", range, yaw);
+                                // RCLCPP_INFO(this->get_logger(), "w_x: %f, w_y: %f", w_x, w_y);
+                                // RCLCPP_INFO(this->get_logger(), "x: %d, y: %d", map_pose.x, map_pose.y);
                         }
                 }
 
-                auto img_map_clone = this->img_map_.clone();
-                RCLCPP_INFO(this->get_logger(), "size: %zu", this->laserscan_points_vector.size());
+                auto img_map_empty_clone = this->img_map_empty_.clone();
+                // RCLCPP_INFO(this->get_logger(), "size: %zu", this->laserscan_points_vector.size());
                 for(int i = 0; i< (int)this->laserscan_points_vector.size(); i++)
                 {
                         MapPose map_pose = this->laserscan_points_vector[i];                        
-                        img_map_clone.at<cv::Vec3b>(map_pose.y, map_pose.x)[0] = 0;
-                        img_map_clone.at<cv::Vec3b>(map_pose.y, map_pose.x)[1] = 0;
-                        img_map_clone.at<cv::Vec3b>(map_pose.y, map_pose.x)[0] = 255;
+                        img_map_empty_clone.at<cv::Vec3b>(map_pose.y, map_pose.x)[0] = 0;
+                        img_map_empty_clone.at<cv::Vec3b>(map_pose.y, map_pose.x)[1] = 0;
+                        img_map_empty_clone.at<cv::Vec3b>(map_pose.y, map_pose.x)[2] = 255;
                 }
 
-                cv::namedWindow("laser_scan", cv::WindowFlags::WINDOW_NORMAL);
-                cv::imshow("laser_scan", img_map_clone);
-                cv::waitKey(10);
+                cv::Mat img_map_clone2 = img_map_empty_clone.clone();
+                cv::Mat gray_img;
+                cv::cvtColor(img_map_clone2, gray_img, cv::COLOR_BGR2GRAY);
+                cv::Mat edges_img;
+                cv::Canny(gray_img, edges_img, this->canny_thr1, this->canny_thr2);
+                std::vector<cv::Vec4i> lines;
+                cv::HoughLinesP(edges_img, lines, this->hough_rho, this->hough_theta, this->hough_thr, this->hough_min_line_length, this->hough_max_line_gap);
+
+                // process lines
+                std::vector<LineInfo>().swap(this->lines_);
+                for (size_t i = 0; i < lines.size(); i++)
+                {
+                        LineInfo line_info;
+                        line_info.line = lines[i];
+                        cv::Point2i pt1, pt2, pt0;
+                        pt1.x = lines[i][0];
+                        pt1.y = lines[i][1];
+                        pt2.x = lines[i][2];
+                        pt2.y = lines[i][3];
+                        if (pt1.x > pt2.x)
+                        {
+                                pt1.x = pt1.x + pt2.x;
+                                pt2.x = pt1.x - pt2.x;
+                                pt1.x = pt1.x - pt2.x;
+
+                                pt1.y = pt1.y + pt2.y;
+                                pt2.y = pt1.y - pt2.y;
+                                pt1.y = pt1.y - pt2.y;
+                        }
+                        line_info.theta = std::atan2(pt2.y - pt1.y, pt2.x - pt1.x);
+
+                        pt0.x = 0, pt0.y = 0;
+                        line_info.rho = area(pt0.x, pt0.y, pt1.x, pt1.y, pt2.x, pt2.y) / distance(pt1.x, pt1.y, pt2.x, pt2.y);
+
+                        this->lines_.push_back(line_info);
+                }
+
+                std::sort(lines_.begin(), lines_.end(), [](LineInfo li1, LineInfo li2){return li1.theta < li2.theta;});
+
+                this->lines_filter(this->lines_);
+                
+                size_t color_size = this->colors.size();
+                cv::Mat img_map_empty_clone2 = this->img_map_empty_.clone();
+                for (size_t i = 0; i < this->lines_.size(); i++)
+                {
+                        cv::Vec4i l = this->lines_[i].line;
+                        cv::line(img_map_empty_clone2, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), this->colors[ i % (color_size + 1)], 1, cv::LINE_AA);
+                }
+
+                // cv::namedWindow("laser_scan", cv::WindowFlags::WINDOW_NORMAL);
+                // cv::imshow("laser_scan", img_map_empty_clone);
+
+                // cv::imshow("Edges", edges_img);
+                cv::namedWindow("Lines", cv::WindowFlags::WINDOW_NORMAL);
+                cv::imshow("Lines", img_map_empty_clone2);
+
+                cv::waitKey(this->imshow_wait);
         }
 }
 
@@ -214,6 +293,8 @@ void WallLineDetection::map_sub_callback_(const OccupancyGridMsg::SharedPtr msg)
         this->map_origin_ = this->map_grid_.info.origin;
         this->map_resolution_ = this->map_grid_.info.resolution;
         RCLCPP_INFO(this->get_logger(), "map width: %d, height: %d", this->map_width_, this->map_height_);
+        RCLCPP_INFO(this->get_logger(), "map origin=> x: %f, y: %f.", this->map_origin_.position.x, this->map_origin_.position.y);
+        RCLCPP_INFO(this->get_logger(), "map resolution: %f", this->map_resolution_);
 
         int channel = 3;
         auto data = this->map_grid_.data;
@@ -259,9 +340,10 @@ void WallLineDetection::map_sub_callback_(const OccupancyGridMsg::SharedPtr msg)
         }
 
         this->img_map_ = cv::Mat(this->map_height_, this->map_width_, CV_8UC3, data_ptr);
-        cv::namedWindow("map", cv::WindowFlags::WINDOW_NORMAL);
+        this->img_map_empty_ = cv::Mat(this->map_height_, this->map_width_, CV_8UC3, cv::Scalar(255, 255, 255));
+        // cv::namedWindow("map", cv::WindowFlags::WINDOW_NORMAL);
         // cv::imshow("map", this->img_map_);
-        // cv::waitKey(0);
+        // cv::waitKey(10);
 }
 
 MapPose WallLineDetection::word_to_picture_bounded(double w_x, double w_y)
@@ -276,6 +358,190 @@ MapPose WallLineDetection::word_to_picture_bounded(double w_x, double w_y)
         map_pose.y = this->map_height_ - 1 - map_pose.y;
 
         return map_pose;
+}
+
+void WallLineDetection::lines_filter(std::vector<LineInfo> &lines_infos)
+{
+        size_t size = lines_infos.size();
+        RCLCPP_INFO(get_logger(), "\nbefore filter, size: %zu", size);
+        int index = 0;
+        for (auto iter = lines_infos.begin(); iter != lines_infos.end(); iter++)
+        {
+                RCLCPP_INFO(get_logger(), "line%d: [(%d, %d), (%d, %d)]", index,
+                        iter->line[0], iter->line[1], iter->line[2], iter->line[3]);
+                index++;
+        }
+        
+        index = 0;
+        for (auto iter1 = lines_infos.begin(); (iter1 != lines_infos.end() - 1) && (iter1 != lines_infos.end()); iter1++)
+        {               
+               RCLCPP_INFO(get_logger(), "----------- %d -----------", index);
+               RCLCPP_INFO(get_logger(), "line%d: [(%d, %d), (%d, %d)]", index++,
+                        iter1->line[0], iter1->line[1], iter1->line[2], iter1->line[3]);
+               int index2 = 0;
+               for (auto iter2 = iter1 + 1; iter2 != lines_infos.end();)
+               {
+                        RCLCPP_INFO(get_logger(), "line%d: [(%d, %d), (%d, %d)]", (index + index2++),
+                                iter2->line[0], iter2->line[1], iter2->line[2], iter2->line[3]);
+                        if (std::abs(iter1->theta - iter2->theta) > this->theta_thr)
+                        {
+                                RCLCPP_INFO(get_logger(), "break for theta threshold.");
+                                break;
+                        }
+                        else if (this->calculate_vertical_distance_dummy(iter1->line, iter2->line) > this->vertical_thr)
+                        {
+                                RCLCPP_INFO(get_logger(), "break for vertical disntance threshold.");
+                                RCLCPP_INFO(get_logger(), "vertical_distance: %f, thre: %f", this->calculate_vertical_distance_dummy(iter1->line, iter2->line), this->vertical_thr);
+                                break;
+                        }
+                        else
+                        {
+                                cv::Point line1_pt1, line1_pt2, line2_pt1, line2_pt2;
+                                double theta1, theta2;
+                                double rho1, rho2;
+                                line1_pt1.x = iter1->line[0];
+                                line1_pt1.y = iter1->line[1];
+                                line1_pt2.x = iter1->line[2];
+                                line1_pt2.y = iter1->line[3];
+                                theta1 = iter1->theta;
+                                rho1 = iter1->rho;
+
+                                line2_pt1.x = iter2->line[0];
+                                line2_pt1.y = iter2->line[1];
+                                line2_pt2.x = iter2->line[2];
+                                line2_pt2.y = iter2->line[3];
+                                theta2 = iter2->theta;
+                                rho2 = iter2->rho;
+
+                                cv::Point line1_pt1_copy, line1_pt2_copy;
+                                cv::Point line2_pt1_copy, line2_pt2_copy;
+                                line1_pt1_copy = line1_pt1;
+                                line1_pt2_copy = line1_pt2;
+                                line2_pt1_copy = line2_pt1;
+                                line2_pt2_copy = line2_pt2;
+
+                                bool merge_occurred = this->merge_lines(line1_pt1, line1_pt2, line2_pt1, line2_pt2, (theta1 + theta2) / 2.0);
+
+                                if(merge_occurred)
+                                {
+                                        (*iter1).line[0] = line1_pt1.x;
+                                        (*iter1).line[1] = line1_pt1.y;
+                                        (*iter1).line[2] = line1_pt2.x;
+                                        (*iter1).line[3] = line1_pt2.y;
+                                        RCLCPP_INFO(get_logger(), "[(%d, %d), (%d, %d)]", line1_pt1.x, line1_pt1.y, line1_pt2.x, line1_pt2.y);
+                                        RCLCPP_INFO(get_logger(), "merge line [(%d, %d), (%d, %d)] and line [(%d, %d), (%d, %d)] to line [(%d, %d), (%d, %d)].",
+                                                line1_pt1_copy.x, line1_pt1_copy.y, line1_pt2_copy.x, line1_pt2_copy.y,
+                                                line2_pt1_copy.x, line2_pt1_copy.y, line2_pt2_copy.x, line2_pt2_copy.y,
+                                                iter1->line[0], iter1->line[1], iter1->line[2], iter1->line[3]);
+                                        RCLCPP_INFO(get_logger(), "line1 => rho: %f, theta: %f", rho1, theta1);
+                                        RCLCPP_INFO(get_logger(), "line2 => rho: %f, theta: %f", rho2, theta2);
+                                        iter2 = lines_infos.erase(iter2);
+                                        if (iter2 == lines_infos.end())
+                                        {
+                                                break;
+                                        }
+                                }
+                                else
+                                {
+                                        RCLCPP_INFO(get_logger(), "line [(%d, %d), (%d, %d)] and line [(%d, %d), (%d, %d)] are different lines.",
+                                                line1_pt1_copy.x, line1_pt1_copy.y, line1_pt2_copy.x, line1_pt2_copy.y,
+                                                line2_pt1_copy.x, line2_pt1_copy.y, line2_pt2_copy.x, line2_pt2_copy.y);
+                                        iter2++;
+                                }
+                        }
+                }
+        }
+        size = lines_infos.size();
+        RCLCPP_INFO(get_logger(), "after filter, size: %zu", size);
+        index = 0;
+        for (auto iter = lines_infos.begin(); iter != lines_infos.end(); iter++)
+        {
+                RCLCPP_INFO(get_logger(), "line%d: [(%d, %d), (%d, %d)]", index,
+                        iter->line[0], iter->line[1], iter->line[2], iter->line[3]);
+                index++;
+        }
+}
+
+bool  WallLineDetection::merge_lines(cv::Point2i& pt1, cv::Point2i& pt2, cv::Point2i pt3, cv::Point2i pt4, double theta)
+{
+        bool ret = false;
+        
+        int delta_x, delta_y;
+        delta_x = std::abs(pt1.x - pt2.x);
+        delta_y = std::abs(pt1.y - pt2.y);
+
+        if (delta_x > delta_y) // major axis => X
+        {
+                if (pt1.x < pt3.x)
+                {
+                        if (pt3.x < pt2.x + this->horizontal_thr / cos(theta))  // Meet the merger conditions
+                        {
+                                // start point, assignment
+                                pt1 = pt1; // just for understand clearly
+                                // end point, assignment
+                                pt2 = pt2.x > pt4.x ? pt2 : pt4;
+                                
+                                ret = true;                          
+                        }
+                        else
+                        {
+                                ret = false; // don't chang anything
+                        }
+                }
+                else
+                {
+                        if (pt1.x < pt4.x + this->horizontal_thr / cos(theta)) // Meet the merger conditions
+                        {
+                                // start point, assignment
+                                pt1 = pt3; 
+                                // end point, assignment
+                                pt2 = pt2.x > pt4.x ? pt2 : pt4; 
+
+                                ret = true;
+                        }
+                        else
+                        {
+                                ret = false; // don't chang anything
+                        }
+                }
+        }
+        else // major axis => Y
+        {
+                if (pt1.y < pt3.y)
+                {
+                        if (pt3.y < pt2.y + this->horizontal_thr / sin(theta))  // Meet the merger conditions
+                        {
+                                // start point, assignment
+                                pt1 = pt1; // just for understand clearly
+                                // end point, assignment
+                                pt2 = pt2.y > pt4.y ? pt2 : pt4;   
+
+                                ret = true;                             
+                        }
+                        else
+                        {
+                                ret = false; // don't chang anything
+                        }
+                }
+                else
+                {
+                        if (pt1.y < pt4.y + this->horizontal_thr / sin(theta)) // Meet the merger conditions
+                        {
+                                // start point, assignment
+                                pt1 = pt3; 
+                                // end point, assignment
+                                pt2 = pt2.y > pt4.y ? pt2 : pt4; 
+
+                                ret = true;
+                        }
+                        else
+                        {
+                                ret = false; // don't chang anything
+                        }
+                }
+        }
+
+        return ret;
 }
 
 } // end of namespace
